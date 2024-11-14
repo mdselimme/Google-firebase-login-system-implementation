0a541fac6d05cd2e48c4eab2dbe236b50660cdbe
mdselimme <mdselimdev@gmail.com>
2024-11-15 01:09:38 +0600

    hide-google-key

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,14 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import app from "../Auth/firebase.init";
 import { useState } from "react";
 
 const Login = () => {
-  const [userData, setUserData] = useState({});
+  const [userData, setUserData] = useState(null);
 
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
@@ -11,15 +16,23 @@ const Login = () => {
   const googleAuthProvider = () => {
     signInWithPopup(auth, provider).then((result) => {
       const user = result.user;
-      console.log(user.photoURL);
+      console.log(user);
       setUserData(user);
+    });
+  };
+
+  const signOutUser = () => {
+    signOut(auth).then((data) => {
+      console.log(data);
     });
   };
 
   return (
     <div>
       <h1>login</h1>
-      <button onClick={googleAuthProvider}>Log in with Google</button>
+      <button onClick={userData.email ? signOutUser : googleAuthProvider}>
+        {userData.email ? "Log Out" : "Log In"}
+      </button>
       <img src={userData.photoURL} alt="" />
       <h1>{userData.displayName}</h1>
       <h3>{userData.email}</h3>
