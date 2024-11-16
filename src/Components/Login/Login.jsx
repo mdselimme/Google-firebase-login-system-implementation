f5afe0219427c01826080029ddb11cda9fb523f9
mdselimme <mdselimdev@gmail.com>
2024-11-16 15:59:28 +0600

    add-login-input form and style

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -6,7 +6,7 @@ import {
 } from "firebase/auth";
 import app from "../Auth/firebase.init";
 import { useState } from "react";
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 
 const Login = () => {
   const [userData, setUserData] = useState({});
@@ -28,6 +28,11 @@ const Login = () => {
     });
   };
 
+  const HandleLogInFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit form");
+  };
+
   return (
     <div>
       <h1>login</h1>
@@ -36,9 +41,39 @@ const Login = () => {
           Sign Out
         </Button>
       ) : (
-        <Button onClick={googleAuthProvider} variant="contained">
-          Log in
-        </Button>
+        <>
+          <div style={{ width: "500px", margin: "0 auto" }}>
+            <form onSubmit={HandleLogInFormSubmit}>
+              <TextField
+                id="outlined-basic"
+                label="Please Enter Your Email"
+                variant="outlined"
+                type="email"
+                fullWidth
+                margin="normal"
+              />
+              <TextField
+                id="outlined-basic"
+                label="Please Enter Your Email"
+                variant="outlined"
+                type="password"
+                fullWidth
+                margin="normal"
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                fullWidth
+                sx={{ margin: "10px", padding: "10px" }}
+              >
+                Log In
+              </Button>
+            </form>
+            <Button onClick={googleAuthProvider} variant="contained">
+              Google Sign In
+            </Button>
+          </div>
+        </>
       )}
       <img src={userData?.photoURL} alt="" />
       <h1>{userData?.displayName}</h1>
